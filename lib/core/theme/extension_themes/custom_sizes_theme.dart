9da0c8d838eec388dd4e99e90616559e2fbc9509
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-11-22 01:43:05 +0500

    add custom sizes theme

---
+++ lib/core/theme/extension_themes/custom_sizes_theme.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter_clean_architecture/core/theme/extensions/custom_sizes.dart';
+
+class CustomSizesTheme {
+  CustomSizesTheme._();
+
+  static const CustomSizes lightTheme = CustomSizes(
+    padding: 20,
+    borderRadius: 15,
+    formHeight: 30,
+  );
+
+  static const CustomSizes darkTheme = CustomSizes(
+    padding: 20,
+    borderRadius: 15,
+    formHeight: 30,
+  );
+}

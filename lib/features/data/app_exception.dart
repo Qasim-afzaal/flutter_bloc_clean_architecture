912b88656c2b484cd37686b12edc852c4265d8b4
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-11-26 02:08:38 +0500

    add app exception

---
+++ lib/features/data/app_exception.dart
@@ -0,0 +1,21 @@
+class AppException implements Exception {
+  final _message;
+  final _prefix;
+
+  AppException([this._message, this._prefix]);
+
+  String toString() {
+    return '$_prefix$_message';
+  }
+}
+
+class FetchDataException extends AppException{
+  FetchDataException([String? message]) : super(message,'Error During Communication');
+}
+
+class BadRequestException extends AppException{
+  BadRequestException([String? message]) : super(message,'Invalid Request');
+}
+class UnauthorisedRequestException extends AppException{
+  UnauthorisedRequestException([String? message]) : super(message,'UnAuthorised Request');
+}

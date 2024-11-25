780cf79a38d7ae0020600fab715d6ef4202826a9
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-11-26 02:07:33 +0500

    add image helper

---
+++ lib/core/utils/helpers/image_helper.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/core/utils/helpers/helper_interface.dart';
+
+class ImageHelper implements Helper {
+  const ImageHelper._();
+  static const ImageHelper instance = ImageHelper._();
+
+  String randomMockImageString() {
+    Random random = Random();
+    int randomNumber = random.nextInt(16 - 1) + 1;
+    return 'assets/images/mock_listings/listing-$randomNumber.png';
+  }
+
+  
+}

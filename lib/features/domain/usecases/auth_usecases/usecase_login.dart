c7d3fc1ca07e2015103c35ce5ec623f0d3800c0e
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-11-29 00:05:21 +0500

    add usecase login

---
+++ lib/features/domain/usecases/auth_usecases/usecase_login.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_clean_architecture/core/usecase/usercase.dart';
+
+import 'package:flutter_clean_architecture/features/domain/entities/user.dart';import 'package:flutter_clean_architecture/features/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../../../../core/theme/error/failure.dart';
+
+class UserSignIn implements UserCase<User, UserSignInParams> {
+  final AuthRepository authRepository;
+  UserSignIn(this.authRepository);
+  @override
+  Future<Either<Failure, dynamic>> call(UserSignInParams params) async {
+    return await authRepository.loginWithEmail(
+        email: params.email, password: params.password);
+  }
+}
+
+class UserSignInParams {
+  final String email;
+  final String password;
+
+  UserSignInParams({required this.email, required this.password});
+}

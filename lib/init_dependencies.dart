04414f22ac165873afbb3dbd382e4d4ed061e6cf
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-12-03 01:15:08 +0500

    update repo

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,9 +1,9 @@
 
 import 'package:flutter_clean_architecture/core/secrets/app_secrets.dart';
 import 'package:flutter_clean_architecture/features/auth_supabase/auth_remote_data_source.dart';
-import 'package:flutter_clean_architecture/features/data/repositories/auth_rep_imp.dart';
-import 'package:flutter_clean_architecture/features/domain/usercases/auth_usecases/usecase_login.dart';
-import 'package:flutter_clean_architecture/features/domain/usercases/auth_usecases/user_sign_up.dart';
+import 'package:flutter_clean_architecture/features/data/repositories/auth_repo_imp.dart';
+import 'package:flutter_clean_architecture/features/domain/usecases/auth_usecases/usecase_login.dart';
+import 'package:flutter_clean_architecture/features/domain/usecases/auth_usecases/user_sign_up.dart';
 import 'package:flutter_clean_architecture/features/presentation/state_managment/auth_bloc/auth_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';

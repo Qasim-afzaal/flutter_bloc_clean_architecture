c9ae1d809d084d485ee761cade11c93b9a7e06d0
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-12-07 15:00:44 +0500

    update model

--- lib/features/data/model/user_model.dart
+++ lib/features/data/model/user_model.dart
@@ -1,12 +1,18 @@
 import 'package:flutter_clean_architecture/features/domain/entities/user.dart';
 
 class UserModel extends User {
-  UserModel({required super.email, required super.id, required super.name});
+  UserModel({
+    required super.email,
+    required super.id,
+    required super.name,
+  });
 
-  factory UserModel.fromJsom(Map<String, dynamic> map) {
+  /// Factory constructor to create a `UserModel` from JSON data.
+  factory UserModel.fromJson(Map<String, dynamic> map) {
     return UserModel(
-        email: map['email'] ?? "",
-        id: map['id'] ?? "",
-        name: map['name'] ?? "");
+      email: map['email'] ?? "",
+      id: map['id'] ?? "",
+      name: map['name'] ?? "",
+    );
   }
 }
